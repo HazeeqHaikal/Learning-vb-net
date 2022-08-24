ee33e53538f199c9500dd3b7b77d35e301f6a76a
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-24 20:49:44 +0800

    arithmetic

---
+++ Arithmetic Operators/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.lblNum2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(300, 122)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(171, 22)
+        Me.txtNum1.TabIndex = 0
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(300, 213)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(171, 83)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(300, 165)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(171, 22)
+        Me.txtNum2.TabIndex = 2
+        '
+        'lblNum1
+        '
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.Location = New System.Drawing.Point(198, 128)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(65, 16)
+        Me.lblNum1.TabIndex = 3
+        Me.lblNum1.Text = "Number 1"
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.Location = New System.Drawing.Point(198, 168)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(65, 16)
+        Me.lblNum2.TabIndex = 4
+        Me.lblNum2.Text = "Number 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents lblNum1 As Label
+    Friend WithEvents lblNum2 As Label
+End Class
---
+++ Arithmetic Operators/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim num1 As Double
+        Dim num2 As Double
+        Dim sumOfNum As Double
+
+        num1 = txtNum1.Text
+        num2 = txtNum2.Text
+
+        sumOfNum = num1 + num2
+        MessageBox.Show(sumOfNum)
+
+        sumOfNum = num1 - num2
+        MessageBox.Show(sumOfNum)
+
+        sumOfNum = num1 * num2
+        MessageBox.Show(sumOfNum)
+
+        sumOfNum = num1 / num2
+        MessageBox.Show(sumOfNum)
+
+        sumOfNum = num1 ^ num2
+        MessageBox.Show(sumOfNum)
+
+        sumOfNum = num1 \ num2
+        MessageBox.Show(sumOfNum)
+
+        sumOfNum = num1 Mod num2
+        MessageBox.Show(sumOfNum)
+
+
+
+    End Sub
+End Class
