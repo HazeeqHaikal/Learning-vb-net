fe663494d6eddf121832e6f46fb07e14dec8651e
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-24 21:15:45 +0800

    complex arithmetic

---
+++ Complex Arithmetic Solution/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim price As Decimal
+        Dim quantity As Integer
+        Dim totalCost As Decimal
+        Dim discount As Decimal
+        Dim postage As Decimal
+        Dim result As Decimal
+
+        price = 35.58
+        quantity = 7
+        discount = 2
+        postage = 3
+        totalCost = (price - discount) * quantity + postage
+
+        MessageBox.Show("RM" & totalCost)
+
+        result = 9 + 8 - 2
+        MessageBox.Show(result)
+
+        result = 9 - 2 + 8
+        MessageBox.Show(result)
+
+        result = 9 + 2 - 8
+        MessageBox.Show(result)
+
+        result = 9 * 2 / 8
+        MessageBox.Show(result)
+
+        result = 9 / 8 * 2
+        MessageBox.Show(result)
+
+        result = 9 * 8 / 2
+        MessageBox.Show(result)
+
+
+
+    End Sub
+End Class
