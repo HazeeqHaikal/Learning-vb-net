be7621a0b97d5cebf2ec591ab0ceda814b0a6591
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-24 21:41:12 +0800

    if else statement

---
+++ If Statement/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCountry = New System.Windows.Forms.TextBox()
+        Me.lblCountry = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtCountry
+        '
+        Me.txtCountry.Location = New System.Drawing.Point(321, 161)
+        Me.txtCountry.Name = "txtCountry"
+        Me.txtCountry.Size = New System.Drawing.Size(166, 22)
+        Me.txtCountry.TabIndex = 0
+        '
+        'lblCountry
+        '
+        Me.lblCountry.AutoSize = True
+        Me.lblCountry.Location = New System.Drawing.Point(114, 167)
+        Me.lblCountry.Name = "lblCountry"
+        Me.lblCountry.Size = New System.Drawing.Size(168, 16)
+        Me.lblCountry.TabIndex = 1
+        Me.lblCountry.Text = "What country are you from?"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(412, 207)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 2
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.lblCountry)
+        Me.Controls.Add(Me.txtCountry)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCountry As TextBox
+    Friend WithEvents lblCountry As Label
+    Friend WithEvents btnSubmit As Button
+End Class
---
+++ If Statement/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim country As String
+        country = txtCountry.Text
+
+        If country.ToLower() = "malaysia" Then
+            MessageBox.Show("Selamat pagi")
+            MessageBox.Show("Jangan lupa makan nasi lemak")
+        ElseIf country.ToLower() = "australia" Then
+            MessageBox.Show("G'day mate")
+        ElseIf country.ToLower() = "japan" Then
+            MessageBox.Show("Konichiwa")
+        Else
+            MessageBox.Show("Hello there from whenever you are")
+        End If
+
+        MessageBox.Show("You are from " & country)
+
+    End Sub
+End Class
