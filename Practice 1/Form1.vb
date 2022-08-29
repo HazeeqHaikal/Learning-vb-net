e8781617e3c95f25d2f1fcf9df94fa1e5aad2429
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-29 21:43:14 +0800

    practice

---
+++ Practice 1/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+
+        Dim max As Integer
+        Dim oddOrEven As String
+        Dim i As Integer
+        Dim combine As String
+
+        max = InputBox("What number do you want to count up to?")
+        oddOrEven = InputBox("Count in odd or even number order?")
+
+        If oddOrEven = "odd" Then
+            For i = 1 To max Step 2
+                combine = combine & i & vbNewLine
+            Next
+        Else
+            For i = 0 To max Step 2
+                combine = combine & i & vbNewLine
+            Next
+        End If
+
+        MessageBox.Show(combine)
+
+    End Sub
+End Class
