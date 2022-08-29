6ea620437fab3ef0eb19f68112fe80d63397fce9
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-29 21:28:13 +0800

    for loop

---
+++ For Next/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim i As Integer
+        Dim combine As String
+
+        For i = 0 To -50 Step -5
+            combine = combine & i & vbNewLine
+        Next
+
+        MessageBox.Show(combine)
+
+    End Sub
+End Class
