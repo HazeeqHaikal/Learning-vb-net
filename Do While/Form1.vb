25b68415af7736a28b68754a9b805de46feeb6a1
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-30 13:26:48 +0800

    do while/until

---
+++ Do While/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnGO_Click(sender As Object, e As EventArgs) Handles btnGO.Click
+
+        Dim i As Integer
+
+        Do
+            i = i + 1
+            MessageBox.Show("Hello " & i)
+        Loop Until i = 5
+
+        MessageBox.Show("Finished loop")
+
+    End Sub
+End Class
