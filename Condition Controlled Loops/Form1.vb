b1fc060dd76bfe6b646778a32165d2967be15345
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-30 16:06:50 +0800

    conditional loop

---
+++ Condition Controlled Loops/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+
+        Dim name As String
+
+        Do While True
+            name = InputBox("Please enter your name")
+            If name = "hazeeq" Then
+                MessageBox.Show("Hi " & name)
+                Exit Do
+            End If
+        Loop
+
+    End Sub
+End Class
