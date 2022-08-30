0d11a65cccb1934f208f59b83f6ad61067e3960c
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-30 14:35:17 +0800

    update do while unitl

--- Do While/Form1.vb
+++ Do While/Form1.vb
@@ -8,6 +8,27 @@
             MessageBox.Show("Hello " & i)
         Loop Until i = 5
 
+        i = 0
+
+        Do Until i = 5
+            i = i + 1
+            MessageBox.Show("Hello " & i)
+        Loop
+
+        i = 0
+
+        Do While i < 5
+            i = i + 1
+            MessageBox.Show("Hello " & i)
+        Loop
+
+        i = 0
+
+        Do
+            i = i + 1
+            MessageBox.Show("Hello " & i)
+        Loop While i < 5
+
         MessageBox.Show("Finished loop")
 
     End Sub
