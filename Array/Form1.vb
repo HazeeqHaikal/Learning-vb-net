817188a53dff0417e6149f84e2da7ef86dd571fa
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-31 21:42:40 +0800

    array

---
+++ Array/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub btnOutput_Click(sender As Object, e As EventArgs) Handles btnOutput.Click
+
+        Dim fruit(5) As String
+        Dim i As Integer
+
+        fruit(0) = "Pisang"
+        fruit(1) = "Mempelam"
+        fruit(2) = "Rambutan"
+        fruit(3) = "Strawberry"
+        fruit(4) = "Dragon fruit"
+        fruit(5) = "Pineapple"
+
+        For i = 0 To 5
+            MessageBox.Show(fruit(i))
+        Next
+
+    End Sub
+End Class
